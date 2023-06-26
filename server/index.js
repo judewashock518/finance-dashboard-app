e248d382f029d62b7952acc512aeb9523a52f5f4
JW <114599894+judewashock518@users.noreply.github.com>
2023-06-26 14:19:58 -0400

    added recharts, connected kpis to mongodb, began incorporating data and themes to chart

--- server/index.js
+++ server/index.js
@@ -32,8 +32,9 @@ mongoose
     })
     .then(async () => {
         app.listen(PORT, () => console.log(`Server Port: ${PORT}`));
-
-        await mongoose.connection.db.dropDatabase();
-        KPI.insertMany(kpis);
+        
+        /* ADD DATA ONE TIME ONLY OR AS NEEDED */
+        // await mongoose.connection.db.dropDatabase();
+        // KPI.insertMany(kpis);
     })
     .catch((error) => console.log(`${error} did not connect`));
