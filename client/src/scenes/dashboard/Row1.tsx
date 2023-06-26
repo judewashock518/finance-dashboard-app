985f4b032bba866cc35b98974665cb8db8524185
JW <114599894+judewashock518@users.noreply.github.com>
2023-06-26 14:40:15 -0400

    created area chart for DashboardBox a, created linearGradients

--- client/src/scenes/dashboard/Row1.tsx
+++ client/src/scenes/dashboard/Row1.tsx
@@ -1,8 +1,16 @@
+// import BoxHeader from "@/components/BoxHeader";
 import DashboardBox from '@/components/DashboardBox';
 import { useGetKpisQuery } from '@/state/api';
 import { useTheme } from "@mui/material";
 import { useMemo } from 'react';
-import { ResponsiveContainer, AreaChart, CartesianGrid, XAxis, YAxis, Tooltip, Area } from "recharts";
+import { 
+  ResponsiveContainer, 
+  AreaChart, 
+  XAxis, 
+  YAxis, 
+  Tooltip, 
+  Area 
+} from "recharts";
 
 type Props = {};
 
@@ -38,11 +46,44 @@ const Row1 = (props: Props) => {
             bottom: 0,
           }}
         >
-          <CartesianGrid strokeDasharray="3 3" />
-          <XAxis dataKey="name" />
-          <YAxis />
+          <defs>
+            <linearGradient id="colorRevenue" x1="0" y1="0" x2="0" y2="1"> 
+              <stop
+                offset="5%"
+                stopColor={palette.primary[300]}
+                stopOpacity={0.5}
+              />
+              <stop
+                  offset="95%"
+                  stopColor={palette.primary[300]}
+                  stopOpacity={0}
+              />
+            </ linearGradient>
+          </defs>
+          <XAxis 
+            dataKey="name" 
+            tickLine={false} 
+            style={{ fontSize: "10px" }} />
+          <YAxis
+            dataKey="name" 
+            tickLine={false} 
+            style={{ fontSize: "10px" }} 
+            domain={[8000, 23000]} />
           <Tooltip />
-          <Area type="monotone" dataKey="revenue" stroke={palette.primary.main} fillOpacity={1} fill="url(#colorRevenue)" />
+          <Area 
+            type="monotone" 
+            dataKey="revenue" 
+            dot={true}
+            stroke={palette.primary.main} 
+            fillOpacity={1} fill="url(#colorRevenue)" 
+          />
+          <Area 
+            type="monotone" 
+            dataKey="expenses" 
+            dot={true}
+            stroke={palette.primary.main} 
+            fillOpacity={1} fill="url(#colorExpenses)" 
+          />
         </AreaChart>
       </ResponsiveContainer>
         </DashboardBox>
