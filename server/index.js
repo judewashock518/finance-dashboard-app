9a1285be77cea7ab8732d68d40610bc53ec8bec2
JW <114599894+judewashock518@users.noreply.github.com>
2023-06-26 21:30:45 -0400

    created transaction and product schemas, product GET request

--- server/index.js
+++ server/index.js
@@ -6,8 +6,11 @@ import dotenv from "dotenv";
 import helmet from "helmet";
 import morgan from "morgan";
 import kpiRoutes from "./routes/kpi.js";
+import productRoutes from "./routes/product.js";
+import Product from "./models/Product.js";
 import KPI from "./models/KPI.js";
-import { kpis } from "./data/data.js";
+import Transaction from "./models/Transaction.js";
+import { kpis, products, transactions } from "./data/data.js";
 
 /*CONFIGURATIONS */
 dotenv.config()
@@ -22,6 +25,7 @@ app.use(cors());
 
 /* ROUTES */
 app.use("/kpi", kpiRoutes);
+app.use("/product", productRoutes);
 
 /* MONGOOSE SETUP */
 const PORT = process.env.PORT || 9000;
@@ -32,9 +36,11 @@ mongoose
     })
     .then(async () => {
         app.listen(PORT, () => console.log(`Server Port: ${PORT}`));
-        
+
         /* ADD DATA ONE TIME ONLY OR AS NEEDED */
-        // await mongoose.connection.db.dropDatabase();
-        // KPI.insertMany(kpis);
+        await mongoose.connection.db.dropDatabase();
+        KPI.insertMany(kpis);
+        Product.insertMany(products);
+        Transaction.insertMany(transactions);
     })
     .catch((error) => console.log(`${error} did not connect`));
